47b0b272fd5ed1c11ffdbb665d31a2cf15b8fd90
Stanko Krtalić <stanko.krtalic@gmail.com>
2015-02-15 23:11:31 +0100

    Router spec

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -2,21 +2,44 @@ require 'rails_helper'
 
 RSpec.describe Interdasting::Router do
   describe '#api_full' do
+    api_full = Interdasting::Router.api_full
+
     it 'returns a hash' do
-      expect(Interdasting::Router.api_full).to be_a(Hash)
+      expect(api_full).to be_a(Hash)
     end
 
     context 'contains all' do
       it 'versions' do
+        expect(api_full.keys).to eq %w(v1 v2 v3)
       end
 
       it 'controllers for every version' do
+        expect(api_full['v1'].keys).to eq %w(api/v1/people)
+        expect(api_full['v2'].keys).to eq %w(api/v2/people api/v2/posts)
+        expect(api_full['v3'].keys).to eq %w(v3/people v3/posts)
       end
 
       it 'actions for every controller' do
+        expect(api_full['v1']['api/v1/people'][:actions].keys.to_set).to(eq(
+          %w(index show new create edit update destroy).to_set
+        ))
+        expect(api_full['v2']['api/v2/people'][:actions].keys.to_set).to(eq(
+          %w(index show new create edit update destroy).to_set
+        ))
+        expect(api_full['v2']['api/v2/posts'][:actions].keys.to_set).to(eq(
+          %w(index show new create edit update destroy).to_set
+        ))
+        expect(api_full['v3']['v3/people'][:actions].keys.to_set).to(eq(
+          %w(index show new create edit update destroy).to_set
+        ))
+        expect(api_full['v3']['v3/posts'][:actions].keys.to_set).to(eq(
+          %w(index show new create edit update destroy).to_set
+        ))
       end
 
       it 'methods for every action for every controller' do
+        # TODO: Figure out a smart way to do this
+        # without writing everything by hand
       end
     end
   end
